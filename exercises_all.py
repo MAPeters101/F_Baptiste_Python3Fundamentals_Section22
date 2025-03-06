731247819e795b567c45a3bb874d9fae6b637d75
Mark Peters <mapeters101@earthlink.net>
2025-03-05 23:31:35 -0500

    Reformat descriptions for all exercises.

--- exercises_all.py
+++ exercises_all.py
@@ -1,15 +1,22 @@
 '''
 Question 1
-Alongside this note book, four CSV files are specified (one is in fact a TSV file).
+Alongside this note book, four CSV files are specified (one is in fact a TSV
+file).
 
-For each file, load it using the CSV module, and find the smallest and largest numbers in the data.
+For each file, load it using the CSV module, and find the smallest and largest
+numbers in the data.
 
-All these files contain just lists of numbers - with the exception of a possible header row
+All these files contain just lists of numbers - with the exception of a
+possible header row
 
 Question 2
-Given this data structure consisting of a list of dictionaries, write a function that will write this data out to a file, where the column headers (in the first row) are based on the dictionary keys, and the values are flattened out to one row per dictionary (under the corresponding column header).
+Given this data structure consisting of a list of dictionaries, write a
+function that will write this data out to a file, where the column headers (in
+the first row) are based on the dictionary keys, and the values are flattened
+out to one row per dictionary (under the corresponding column header).
 
-Note that not all dictionaries contain all the same keys, nor are the keys necessarily in the same order when present.
+Note that not all dictionaries contain all the same keys, nor are the keys
+necessarily in the same order when present.
 
 For "missing" values, your function should just write an empty string.
 
@@ -25,5 +32,6 @@ a,b,c,d,e
 1_a,1_b,1_c,,,
 ,,2_c,2_d,
 3_a,,3_c,,3_e
-The order of the columns and rows is not important - as long as they match up with respective column headers.
+The order of the columns and rows is not important - as long as they match up
+with respective column headers.
 '''
