064bb4c3ad89d0039d5d270768f2cce5970af806
Mark Peters <mapeters101@earthlink.net>
2025-03-05 23:32:50 -0500

    Descriptions for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,12 @@
+'''
+Question 1
+Alongside this note book, four CSV files are specified (one is in fact a TSV
+file).
+
+For each file, load it using the CSV module, and find the smallest and largest
+numbers in the data.
+
+All these files contain just lists of numbers - with the exception of a
+possible header row
+'''
+
